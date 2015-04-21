c0e92426460bb1192e3f3f57983fd0bca7b78924
hoey <nznial@gmail.com>
2015-04-22 01:16:27 +0900

    Commit with testing
    Muddling though.
    Keep some changes.

---
+++ testrun.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This script is for testing.
+# Written because of my bad memory
+
+# --detach make current terminal detached with server process
+# killl -9 {PID} for stopping
+bundle exec jekyll serve --detach
+
+# Open browser
+open "http://localhost:4000"
