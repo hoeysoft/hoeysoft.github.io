edac5d4a528ba6a64dcee752af51c6c59eb079aa
hoey <nznial@gmail.com>
2015-04-26 21:42:48 +0900

    testrun.sh --drafts flag added.

--- testrun.sh
+++ testrun.sh
@@ -5,7 +5,7 @@
 
 # --detach make current terminal detached with server process
 # killl -9 {PID} for stopping
-bundle exec jekyll serve --detach
+bundle exec jekyll serve --drafts --detach
 
 # Open browser
 open "http://localhost:4000"
